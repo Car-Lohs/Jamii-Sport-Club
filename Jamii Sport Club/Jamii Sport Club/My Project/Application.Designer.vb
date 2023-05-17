706ee3051c35df009c258d4197e1b04df5eb3c65
Carlos <carlosonyango3930@gmail.com>
2023-05-17 17:31:43 +0300

    Tables Design

--- Jamii Sport Club/Jamii Sport Club/My Project/Application.Designer.vb
+++ Jamii Sport Club/Jamii Sport Club/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Jamii_Sport_Club.Formhome
+            Me.MainForm = Global.Jamii_Sport_Club.Formsplash
         End Sub
     End Class
 End Namespace
