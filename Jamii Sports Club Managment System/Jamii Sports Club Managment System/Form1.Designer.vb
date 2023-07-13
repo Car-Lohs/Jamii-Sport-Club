40de473be53cf51efb0cc8c4a314159de39875d2
Carlos <carlosonyango3930@gmail.com>
2023-07-13 23:01:44 +0300

    Fresh Start

---
+++ Jamii Sports Club Managment System/Jamii Sports Club Managment System/Form1.Designer.vb
@@ -0,0 +1,46 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class LoginForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.GunaElipse1 = New Guna.UI.WinForms.GunaElipse(Me.components)
+        Me.SuspendLayout()
+        '
+        'GunaElipse1
+        '
+        Me.GunaElipse1.Radius = 50
+        Me.GunaElipse1.TargetControl = Me
+        '
+        'LoginForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(745, 503)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "LoginForm"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GunaElipse1 As Guna.UI.WinForms.GunaElipse
+
+End Class
